587856b0607c9328298666e31ac02528a713212b
tjallen27 <tjallen91@hotmail.co.uk>
2018-04-12 20:32:05 +0100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Checkout from './Checkout';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          <Checkout
+            name={'The Road to learn React'}
+            description={'Only the Book'}
+            amount={1}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
